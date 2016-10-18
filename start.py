d7cc4aca87729d31dd98020f79158b25a5e94d84
Will Ronchetti <wrr33@cornell.edu>
2016-10-18 13:14:10 -0400

    migrated to pexpect, created main and solver class

--- start.py
+++ start.py
@@ -3,14 +3,38 @@ import time
 import os
 import signal
 
-started = False
-process0 = None
+# Need to download pexpect here: https://pexpect.readthedocs.io/en/stable/index.html
+import pexpect
 
-while True:
-    if not started:
-        print "starting..."
-        started = True
-        process0 = subprocess.Popen("emacs RET -batch -l dunnet", shell=True, stdin=subprocess.PIPE,stderr=subprocess.STDOUT)
-    else:
-        time.sleep(2)
-        process0.communicate("go east")
+started = False
+engine = None
+
+
+class Solver(object):
+
+	def __init__(self, instructions):
+		self.instructions = instructions
+		self.engine = None
+
+
+	def spawn_solver(self):
+		while True:
+    		if not started:
+        		print "Starting..."
+        		started = True
+        		self.engine = pexpect.spawn('emacs RET -batch -l dunnet')
+    		else:
+        		time.sleep(2)
+        		self.engine.sendline("go east")
+
+
+
+def main():
+	instr = raw_input("Enter instructions for solver: ")
+	Solver = Solver(None, instr)
+	Solver.spawn_solver()
+
+
+
+if __name__ == '__main__':
+	main()
